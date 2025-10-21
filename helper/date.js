9a21999b4fc4924d2df56ca365f8ad672314b5bd
emirralkn60 <emirralkn@gmail.com>
2025-10-21 16:42:46 +0300

    OgrİcUyg
    OgrİcUyg

---
+++ helper/date.js
@@ -0,0 +1,10 @@
+export function getFormattedDate(date) {
+  const yyyy = date.getFullYear();
+  const mm = String(date.getMonth() + 1).padStart(2, '0');
+  const dd = String(date.getDate()).padStart(2, '0');
+  return `${yyyy}-${mm}-${dd}`;
+}
+
+export function getLastWeek(date,days){
+    return new Date(date.getFullYear(),date.getMonth(),date.getDate()-days);
+}
